c6e797c8eb9a244ce2e98e50f6226efed00c9883
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-12 08:16:46 +0545

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,3 @@
-import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -94,41 +93,5 @@ export const logout = async (req, res) => {
   } catch (err) {
     console.log("Error in logout controller: " + err.message);
     res.status(500).json({ message: "Internal Server Error" });
-  }
-};
-
-// -------------- Update Profile controller -------------------
-export const updateProfile = async (req, res) => {
-  try {
-    const { profilePic } = req.body;
-    const userId = req.user._id;
-
-    if (!profilePic)
-      return res.status(400).json({ message: "Profile picture is required" });
-
-    const uploadResponse = await cloudinary.uploader.upload(profilePic);
-
-    const updatedUser = await User.findByIdAndUpdate(
-      userId,
-      {
-        profilePic: uploadResponse.secure_url,
-      },
-      { new: true }
-    );
-
-    return res.status(200).json(updatedUser);
-  } catch (err) {
-    console.log("Error in logout controller: " + err.message);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
-};
-
-// -------------- Check Auth controller -------------------
-export const checkAuth = async (req, res) => {
-  try {
-    return res.status(200).json(req.user);
-  } catch (err) {
-    console.log("Error in checkAuth controller: " + err.message);
-    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
