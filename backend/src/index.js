9632862caa05621d98825ad5d3f651124f02cfd6
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-13 13:22:53 +0545

    Increase json size limit

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,8 +12,10 @@ const app = express();
 
 const PORT = process.env.PORT;
 
-app.use(express.json());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 app.use(cookieParser());
+
 app.use(
   cors({
     origin: "http://localhost:5173",
