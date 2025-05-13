2341a34dd5b773c5134c3176289ac5e8f86c5d8c
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-13 12:56:58 +0545

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -39,6 +39,7 @@ export const signup = async (req, res) => {
         fullName: newUser.fullName,
         email: newUser.email,
         profilePic: newUser.profilePic,
+        createdAt: newUser.createdAt,
       });
     } else {
       return res.status(400).json({ message: "Invalid user data" });
@@ -78,6 +79,7 @@ export const login = async (req, res) => {
       fullName: user.fullName,
       email: user.email,
       profilePic: user.profilePic,
+      createdAt: user.createdAt,
     });
   } catch (err) {
     console.log("Error in login controller: " + err.message);
