93ee39892c19421c3b762bda8d6abaf2e40690f0
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-14 10:03:29 +0545

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,9 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const app = express();
 
 const PORT = process.env.PORT;
 
@@ -27,7 +27,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(PORT, () => {
-  console.log("App is listening on port: " + PORT);
+server.listen(PORT, () => {
+  console.log("Server is listening on port: " + PORT);
   connectDB();
 });
