e8cc4405e248377842db460cea81744a4133247d
Prabesh Shrestha <124581918+1prabess@users.noreply.github.com>
2025-05-14 10:03:22 +0545

    Update message.controller.js

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import cloudinary from "../lib/cloudinary.js";
+import { getReceiverSocketId, io } from "../lib/socket.js";
 import Message from "../models/message.model.js";
 
 // -------------- Get messages controller -------------------
@@ -44,7 +45,10 @@ export const sendMessage = async (req, res) => {
 
     await newMessage.save();
 
-    // todo: Socket.IO
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (err) {
